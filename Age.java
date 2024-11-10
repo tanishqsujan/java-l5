d8758d330619cea2a9dbdfff2fbddcb19a715b6f
tanishqsujan <tanishq.kothamasu@gmail.com>
2024-11-10 11:17:49 +0400

    Age

---
+++ Age.java
@@ -0,0 +1,12 @@
+class Age{
+    int age;
+    Age(int age){
+        //age=age
+        this.age=age;
+    }
+
+    public static void main(String[] args){
+        Age a= new Age(24);
+        System.out.println("Age: " +a.age);
+    }
+}
